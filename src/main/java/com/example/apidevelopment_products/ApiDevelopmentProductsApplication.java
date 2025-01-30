9d13f490f2e96bfb384618957ea58db2b079b2a3
Akshay Patil <studyakshaystudy@outlook.com>
2025-01-31 03:10:55 +0800

    first commit

---
+++ src/main/java/com/example/apidevelopment_products/ApiDevelopmentProductsApplication.java
@@ -0,0 +1,13 @@
+package com.example.apidevelopment_products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDevelopmentProductsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiDevelopmentProductsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apidevelopment_products/ApiDevelopmentProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apidevelopment_products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDevelopmentProductsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
